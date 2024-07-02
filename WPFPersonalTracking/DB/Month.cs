0481d8c449983f19126ff2e4432b2083e6d8b67c
thanhnhan <nhandthe17016@fpt.edu.vn>
2024-07-02 10:33:26 +0700

    Add project files.

---
+++ WPFPersonalTracking/DB/Month.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPFPersonalTracking.DB;
+
+public partial class Month
+{
+    public int Id { get; set; }
+
+    public string? MonthName { get; set; }
+
+    public virtual ICollection<Salary> Salaries { get; set; } = new List<Salary>();
+}
